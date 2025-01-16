fc3b0730837e5490d19b492280cb39fe98a2282d
Lucas Pereira de Andrade <134231925+LucasAndrade1605@users.noreply.github.com>
2025-01-16 15:53:26 -0400

    Adicionando a atividade do módulo 05

---
+++ Mod 05/Mod04/src/mod04/Main.java
@@ -0,0 +1,16 @@
+package mod04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		String ambiente = args[0];
+		
+		if (ambiente.equals("DEV")) {
+			System.out.println("DEV");
+		}
+
+	}
+
+}
